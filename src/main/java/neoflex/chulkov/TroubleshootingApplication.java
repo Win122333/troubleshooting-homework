36b2d9691c45bb862ec468527cf2dd5a933aec59
Win122333 <chulkov_a_d@sc.vsu.ru>
2026-05-01 19:19:37 +0300

    Initial commit

---
+++ src/main/java/neoflex/chulkov/TroubleshootingApplication.java
@@ -0,0 +1,11 @@
+package neoflex.chulkov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroubleshootingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TroubleshootingApplication.class, args);
+    }
+}
